7a9698d7ca1274116f05265be5d916952442b1ac
Jared Smith <jsnimbus2000@gmail.com>
2024-03-09 16:42:44 -0700

    added func data_cleaner

--- vol3/data_cleaner.py
+++ vol3/data_cleaner.py
@@ -3,20 +3,12 @@ import numpy as np
 from io import StringIO
 from html.parser import HTMLParser
 import string
+import pickle
 import re
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk import download
 download('stopwords')
-
-
-
-data_dir = '/home/tylerc/dat/school/acme/'
-twitter_df = pd.read_csv(data_dir + "twitter_data.csv", index_col=0)
-imdb_df = pd.read_csv(data_dir + "imdb_data.csv", index_col=0)
-yelp_df = pd.read_csv(data_dir + "yelp_data.csv", index_col=0)
-
-
 
 # Removes punctuation and numbers (by character) and returns as a single string
 def remove_punctuation(text):
@@ -61,20 +53,25 @@ def remove_stopwords(text, root='stem'):
         return [wnl.lemmatize(word) for word in text if word not in stopword_lst]
 
 
+def data_cleaner(filthy_data, root='stem'):
+    # Apply cleaning functions to the data
+    clean_data = filthy_data['text'].progress_apply(lambda x: remove_stopwords( # Remove stopwords and shorten to root words
+                                                    tokenize(                   # Split message into a list
+                                                    remove_punctuation(         # Remove punctuation and numbers
+                                                    remove_urls(                # Remove URLs
+                                                    strip_tags(x)               # Remove HTML tags
+                                                    )).lower()), root))
 
-# Apply cleaning functions to the data
-twitter_subset = twitter_df.sample(10000)
-clean_data = twitter_subset['text'].progress_apply(lambda x: remove_stopwords(   # Remove stopwords and shorten to root words
-                                                tokenize(           # Split message into a list
-                                                remove_punctuation( # Remove punctuation and numbers
-                                                remove_urls(        # Remove URLs
-                                                strip_tags(x)       # Remove HTML tags
-                                                )).lower())))
-
-# Turn each mesage into a sequence of unique indices
-#   that correspond to a given word
-words = []
-for message in clean_data:
-    words = list(set(words + message))
-
-seq_data = [[words.index(word) for word in message] for message in clean_data]
+    # Create our corpus of words
+    corpus = []
+    for message in clean_data:
+        corpus = list(set(corpus + message))
+    # Turn each message into a sequence of unique indices
+    #   that correspond to a given word
+    seq_data = [[corpus.index(word) for word in message] for message in clean_data]
+    # Format data as a DataFrame with the original target attached.
+    clean_df = pd.DataFrame()
+    clean_df['text'] = seq_data
+    clean_df['target'] = filthy_data['target']
+    
+    return clean_df, corpus
