439595b43c4882b631818646c6f6e6aa2f0701ae
Jared Smith <jsnimbus2000@gmail.com>
2024-03-09 18:16:02 -0700

    added lemmatizer and url/user tags

--- vol3/data_cleaner.py
+++ vol3/data_cleaner.py
@@ -1,21 +1,46 @@
 import pandas as pd
-import numpy as np
 from io import StringIO
 from html.parser import HTMLParser
 import string
-import pickle
 import re
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk import download
 download('stopwords')
 
+# TODO: Correcting misspelled words
+#   Not feasible. For every single word, we would
+#   have to compute the distance between it and 8,320 
+#   other words which causes the temporal complexity
+#   to be enormous
+
+# Define stopwords and remove them from the list
+# Also reduce words to the root word
+def remove_stopwords(text, root='stem'):
+    stopword_lst = stopwords.words('english')
+    if root == 'stem':
+        ps = PorterStemmer()
+        return [word if (word == 'acmeuser') or (word == 'acmeurl') else ps.stem(word) for word in text if word not in stopword_lst]
+    elif root == 'lemmatize':
+        wnl = WordNetLemmatizer()
+        return [word if (word == 'acmeuser') or (word == 'acmeurl') else wnl.lemmatize(word) for word in text if word not in stopword_lst]
+
+# Splits the message on one or more non-word character
+# Returns as a list
+def tokenize(text):
+    return re.split("\W+", text)
+
 # Removes punctuation and numbers (by character) and returns as a single string
 def remove_punctuation(text):
     return ''.join([char for char in text if (char not in string.punctuation) and (not char.isdigit())])
 
-# Remove URLs from a string
-def remove_urls(text, replacement_text=""):
+# Replace usernames from a string with acmeuser
+def remove_users(text, replacement_text="acmeuser"):
+    user_pattern = re.compile(r'@\S+')
+    return user_pattern.sub(replacement_text, text)
+
+# Replace URLs from a string with acmeurl
+def remove_urls(text, replacement_text="acmeurl"):
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     return url_pattern.sub(replacement_text, text)
 
@@ -36,31 +61,15 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
-# Splits the message on one or more non-word character
-# Returns as a list
-def tokenize(text):
-    return re.split("\W+", text)
-    
-# Define stopwords and remove them from the list
-# Also reduce words to the root word
-def remove_stopwords(text, root='stem'):
-    stopword_lst = stopwords.words('english')
-    if root == 'stem':
-        ps = PorterStemmer()
-        return [ps.stem(word) for word in text if word not in stopword_lst]
-    elif root == 'lemmatize':
-        wnl = WordNetLemmatizer()
-        return [wnl.lemmatize(word) for word in text if word not in stopword_lst]
-
-
 def data_cleaner(filthy_data, root='stem'):
     # Apply cleaning functions to the data
     clean_data = filthy_data['text'].progress_apply(lambda x: remove_stopwords( # Remove stopwords and shorten to root words
                                                     tokenize(                   # Split message into a list
                                                     remove_punctuation(         # Remove punctuation and numbers
-                                                    remove_urls(                # Remove URLs
+                                                    remove_users(               # Replace users
+                                                    remove_urls(                # Replace URLs
                                                     strip_tags(x)               # Remove HTML tags
-                                                    )).lower()), root))
+                                                    ))).lower()), root))
 
     # Create our corpus of words
     corpus = []
@@ -73,5 +82,5 @@ def data_cleaner(filthy_data, root='stem'):
     clean_df = pd.DataFrame()
     clean_df['text'] = seq_data
     clean_df['target'] = filthy_data['target']
-    
+
     return clean_df, corpus
