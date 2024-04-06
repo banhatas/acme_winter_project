3a184ce4153a2819042ec2903ec36d73cb01c5de
Addison <42590715+AddisonFP@users.noreply.github.com>
2024-04-05 23:59:45 -0600

    L on the bang bang conditions

--- vol4/JaAdLiTy.py
+++ vol4/JaAdLiTy.py
@@ -8,9 +8,9 @@ from matplotlib import pyplot as plt
 def standing():
 
     # initialize the params
-    n=200 
+    n=200 #20 fps so 200=10s
     loop = tqdm(total=n)
-    alpha1, alpha2, alpha3, alpha4 = 0, 9.81, 5*π/180, 7*π/180
+    alpha1, alpha2, alpha3, alpha4 = 5, 9.81, 5*π/180, 7*π/180
     endscale = 1
     mu1, mu2, mu3, mu4 = 1, 1, 1, 1
     l1, l2 = 1, 1
@@ -23,27 +23,35 @@ def standing():
         ind   0  1  2   3   4   5   6   7
             ([x, y, θ1, θ2, φ1, φ2, p1, p2])'
         """
+        n = len(y[0])
+        #pre allocate some of the variables
+        dtheta1 = np.zeros(n)
+        dtheta2 = np.zeros(n)
+        dphi1 = np.zeros(n)
+        dphi2 = np.zeros(n)
+        dx, dy = np.zeros(n), np.zeros(n)
+        u1, u2, u3, u4 = np.zeros(n), np.zeros(n), np.zeros(n), np.zeros(n)
+
         #check whether each leg is on the ground
         cond1 = y[1]-(l1*np.cos(y[2])+l2*np.cos(y[4])) == 0
         cond2 = y[1]-(l1*np.cos(y[3])+l2*np.cos(y[5])) == 0
 
-        #make the derivative of the control bangbang
-        if y[6]*l1*beta - y[7]*l2*2/π*y[2] < 0:
-            dtheta1 = alpha3
-        else:
-            dtheta1 = -alpha3
-        if y[6]*l1*beta - y[7]*l2*2/π*y[3] < 0:
-            dtheta2 = alpha3
-        else:
-            dtheta2 = -alpha3
-        if y[6]*l1*beta - y[7]*l2*2/π*y[4] < 0:
-            dphi1 = alpha4
-        else:
-            dphi1 = -alpha4
-        if y[6]*l1*beta - y[7]*l2*2/π*y[5] < 0:
-            dphi2 = alpha4
-        else:
-            dphi2 = -alpha4
+        #make the derivative of all 4 controls bangbang
+        mask = y[6]*l1*beta - y[7]*l2*2/π*y[2] < 0
+        dtheta1[mask] = alpha3
+        dtheta1[~mask] = -alpha3
+
+        mask = y[6]*l1*beta - y[7]*l2*2/π*y[3] < 0
+        dtheta2[mask] = alpha3
+        dtheta2[~mask] = -alpha3
+
+        mask = y[6]*l1*beta - y[7]*l2*2/π*y[4] < 0
+        dphi1[mask] = alpha4
+        dphi1[~mask] = -alpha4
+
+        mask = y[6]*l1*beta - y[7]*l2*2/π*y[5] < 0
+        dphi2[mask] = alpha4
+        dphi2[~mask] = -alpha4
 
         # define different parts of the state evolution
         dx1 = l2*dphi1*np.cos(y[4]) + l1*dtheta1*np.cos(y[2])
@@ -52,31 +60,35 @@ def standing():
         dy2 = -l2*dphi2*np.sin(y[5]) - l1*dtheta2*np.sin(y[3])
         
         #define the state evolution equations
-        if cond1 and cond2: dx, dy = dx1+dx2, dy1+dy2
-        elif cond1: dx, dy = dx1, dy1
-        elif cond2: dx, dy = dx2, dy2
-        else: dx, dy = 0, -alpha2
+        for i in range(n):
+            if cond1[i] and cond2[i]: dx[i], dy[i] = -dx1[i]-dx2[i], -dy1[i]-dy2[i] ###### changed all x' and y' signs to - didn't change anything but may want to leave in
+            elif cond1[i]: dx[i], dy[i] = -dx1[i], -dy1[i]
+            elif cond2[i]: dx[i], dy[i] = -dx2[i], -dy2[i]
+            else: dx[i], dy[i] = 0, -alpha2
 
         val = mu1 - mu2 + mu3 - mu4
         #define how the control evolves
-        if cond1:
-            u1 = (2*alpha3+l1*dtheta1*y[6])**-1*(val - y[7]*l1*dtheta1*beta)
-            u3 = (2*alpha4+l2*dphi1*y[6])**-1*(-mu1+mu2 - y[7]*l2*dphi1*beta)
-        else:
-            u1 = 1/(2*alpha3)*val
-            u3 = 1/(2*alpha4)*(-mu1+mu2)
-        if cond2:
-            u2 = (2*alpha3+l1*dtheta2*y[6])**-1*(val - y[7]*l1*dtheta2*beta)
-            u4 = (2*alpha4+l2*dphi2*y[6])**-1*(-mu1+mu2 - y[7]*l2*dphi2*beta)
-        else:
-            u2 = 1/(2*alpha3)*val
-            u4 = 1/(2*alpha4)*(-mu1+mu2)      
+        u1[cond1] = (2*alpha3+l1*dtheta1[cond1]*y[6][cond1])**-1*(val - y[7][cond1]*l1*dtheta1[cond1]*beta)
+        u3[cond1] = (2*alpha4+l2*dphi1[cond1]*y[6][cond1])**-1*(-mu1+mu2 - y[7][cond1]*l2*dphi1[cond1]*beta)
+        u1[~cond1] = 1/(2*alpha3)*val
+        u3[~cond1] = 1/(2*alpha4)*(-mu1+mu2)
+
+        u2[cond2] = (2*alpha3+l1*dtheta2[cond2]*y[6][cond2])**-1*(val - y[7][cond2]*l1*dtheta2[cond2]*beta)
+        u4[cond2] = (2*alpha4+l2*dphi2[cond2]*y[6][cond2])**-1*(-mu1+mu2 - y[7][cond2]*l2*dphi2[cond2]*beta)
+        u2[~cond2] = 1/(2*alpha3)*val
+        u4[~cond2] = 1/(2*alpha4)*(-mu1+mu2)      
         
         loop.update()
-        return np.array([dx, dy, u1, u2, u3, u4, 0, (y[1]+alpha1)**-2])
+        return np.array([dx, dy, u1, u2, u3, u4, np.zeros(n), 4*(alpha1*y[1]-alpha1*targety)**3])
     
+    """bc i've tested:
+    return np.array([ya[0]-1, ya[1]-1.5, ya[2]+π/90, ya[3]-π/6, ya[4]+π/2, ya[5]+π/2, yb[1]-targety, yb[6], yb[7]+endscale*2*(yb[1]-targety)])
+    return np.array([ya[0]-1, ya[1]-1.5, ya[2]+π/90, ya[3]-π/6, ya[4]+π/2, ya[5]+π/2, yb[6], yb[7]+endscale*2*(yb[1]-targety)])
+    ^this one 'worked' gave me something interesting but went below the x axis
+
+    """
     def bc(ya, yb):
-        return np.array(ya[0]-1, ya[1]-1.5, ya[2]+π/90, ya[3]-π/6, ya[4]+π/2, ya[5]+π/2, yb[1]-targety, yb[6], yb[7]+endscale*2*(yb[1]-targety))
+        return np.array([ya[0]-1, ya[1]-1.5, ya[2]+π/90, ya[3]-π/6, ya[4]+π/2, ya[5]+π/2, yb[6], yb[7]])
     
     #use solve bvp to get a solution
     y0 = np.ones((8,n))
@@ -88,10 +100,11 @@ def standing():
 
 if __name__ == '__main__':
     sol = standing()
-    domain = np.linspace(0, 10, 200)
+    n = len(sol.y[0])
+    domain = np.linspace(0, 10, n)
     plt.subplot(211)
     plt.title('state')
-    plt.plot(sol.y[0], sol.y[1])
+    plt.scatter([sol.y[0][0], sol.y[0][-1]], [sol.y[0][1], sol.y[1][-1]], c=['r', 'g'])
     plt.subplot(212)
     plt.title('controls')
     plt.plot(domain, sol.y[2], label = 'θ1')
